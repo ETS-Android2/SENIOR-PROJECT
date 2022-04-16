a905f75608cc822d2e8ad4deb1c579746b90eeb0
Tolga Çobanoğlu <76070986+tolgacobanoglu@users.noreply.github.com>
2022-04-16 12:20:48 +0300

    Add files via upload

---
+++ Testing Documents/TestProject/src/crypto/SHA256.java
@@ -0,0 +1,36 @@
+package crypto;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+    private static byte[] generateSHA256(String textToHash) throws NoSuchAlgorithmException {
+
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(textToHash.getBytes(Charset.defaultCharset()));
+
+        return messageDigest.digest();
+    }
+
+    private static final String HEXADECIMALS = "0123456789abcdef";
+
+    private static String convertByteArrayToHexString(byte[] byteArray) {
+
+        final StringBuilder hexText = new StringBuilder(2 * byteArray.length);
+
+        for (byte byteElement : byteArray) {
+            hexText
+                    .append(HEXADECIMALS.charAt((byteElement & 0xF0) >> 4))
+                    .append(HEXADECIMALS.charAt((byteElement & 0x0F)));
+        }
+
+        return hexText.toString();
+    }
+
+
+    public static String hashString(String text) throws NoSuchAlgorithmException {
+        String hashedValue = convertByteArrayToHexString(generateSHA256(text));
+        return hashedValue;
+    }
+}
